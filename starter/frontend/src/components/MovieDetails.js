928735b69c7b88c09f51eeebe58ea7514aa6026e
unknown <deekshithareddy2002@gmail.com>
2026-08-22 22:17:58 +0530

    Implement frontend and backend CI/CD pipelines

--- starter/frontend/src/components/MovieDetails.js
+++ starter/frontend/src/components/MovieDetails.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 
 function MovieDetail({ movie }) {
   const [details, setDetails] = useState(null);
+
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies/${movie.id}`).then((response) => {
-      setDetails(response.data);
-    });
+    axios
+      .get(`${process.env.REACT_APP_MOVIE_API_URL}/movies/${movie.id}`)
+      .then((response) => {
+        setDetails(response.data);
+      });
   }, [movie]);
 
   return (
@@ -16,5 +20,13 @@ function MovieDetail({ movie }) {
     </div>
   );
 }
+
+MovieDetail.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    title: PropTypes.string,
+    description: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieDetail;

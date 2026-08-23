befead3d81ad9196e87f8c2202eaa7a7a863a132
unknown <deekshithareddy2002@gmail.com>
2026-08-23 22:53:49 +0530

    Format MovieList for frontend build

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -6,14 +6,15 @@ function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get(process.env.REACT_APP_MOVIE_API_URL + '/movies')
-  .then((response) => {
-    setMovies(response.data?.movies || []);
-  })
-  .catch((error) => {
-    console.error('Failed to load movies:', error);
-    setMovies([]);
-  });
+    axios
+      .get(process.env.REACT_APP_MOVIE_API_URL + '/movies')
+      .then((response) => {
+        setMovies(response.data?.movies || []);
+      })
+      .catch((error) => {
+        console.error('Failed to load movies:', error);
+        setMovies([]);
+      });
   }, []);
 
   return (
